1a98a99667315f8e9d544f3bfca920494ef6450c
lolziac <lolziac@ll.(none)>
2011-10-26 12:16:40 +0200

    Category Validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,12 @@ class Category < ActiveRecord::Base
 	has_many :products
 	
 	validates_presence_of :name
-	
+  validates :position, :presence => true, :length => { :maximum => 350 }
+  validates :visible, :presence => true
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+
 	scope :visible, where(:visible => true) 
 	scope :invisible, where(:visible => false)
 	scope :sorted, order('categories.position ASC') 
@@ -13,7 +18,4 @@ class Category < ActiveRecord::Base
 	:url  => "/assets/categories/:id/:style/:basename.:extension",
 	:path => ":rails_root/public/assets/categories/:id/:style/:basename.:extension"
 
-	validates_attachment_presence :photo
-	validates_attachment_size :photo, :less_than => 5.megabytes
-	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 end
